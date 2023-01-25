99403d567ed9e77ff391c289cde2e59139aeca8f
Victor Olamide <akinnusivictor098@gmail.com>
2023-01-25 23:27:05 +0100

    Done

--- URLShortener/src/main/java/com/volacode/URLShortener/services/URLServiceImpl.java
+++ URLShortener/src/main/java/com/volacode/URLShortener/services/URLServiceImpl.java
@@ -5,12 +5,11 @@ import com.volacode.URLShortener.data.repositories.URLRepository;
 import com.volacode.URLShortener.dtos.reponses.GetLongURLResponse;
 import com.volacode.URLShortener.dtos.reponses.GetShortURLResponse;
 import com.volacode.URLShortener.dtos.requests.GetShortURLRequest;
+import com.volacode.URLShortener.exceptions.InvalidURLException;
 import com.volacode.URLShortener.utils.IDConverter;
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 
 @Service
@@ -20,10 +19,11 @@ public class URLServiceImpl implements URLService{
     private  final URLRepository urlRepository;
     @Override
     public GetShortURLResponse getShortURL(GetShortURLRequest getURLRequest) {
+        isValidURL(getURLRequest.getLongURL());
 
         URLLink urlLink = new URLLink();
         urlLink.setLink(getURLRequest.getLongURL());
-        urlLink.setId(getId());
+        urlLink.setId(generateID());
         urlRepository.save(urlLink);
 
         Dotenv dotenv = Dotenv.configure().directory("C:\\Users\\Olamide\\Desktop\\URLShortener\\URLShortener\\src\\main\\java\\com\\volacode\\URLShortener\\assets\\.env").filename(".env").load();
@@ -33,26 +33,38 @@ public class URLServiceImpl implements URLService{
         return response;
     }
 
-    private String getId() {
-        return IDConverter.convertRequestID(urlRepository.count());
-    }
-
     @Override
     public GetLongURLResponse getLongURL(String shortURL) {
-        String id =getRequestId(shortURL);
+        String id = getRequestID(shortURL);
+
         GetLongURLResponse response = new GetLongURLResponse();
         response.setLongURL(urlRepository.findURLLinkById(id).getLink());
-
         return response;
     }
 
-    private String getRequestId(String shortURL) {
+    private String getRequestID(String shortURL) {
         String[] compArray = shortURL.split("/");
-        return  compArray[compArray.length -1];
+        return compArray[compArray.length - 1];
     }
 
     @Override
     public Long numOfURLs() {
-        return  urlRepository.count();
+        return urlRepository.count();
     }
+
+    private String generateID() {
+        return IDConverter.convertRequestID(urlRepository.count());
+
+    }
+
+
+    private void isValidURL(String longURL) {
+        if (!longURL.matches("^https?:\\/\\/(?:www.)?[-a-zA-Z\\d@:%._\\+~#=]{1,256}" +
+                "\\.[a-zA-Z\\d9()]{1,6}\\b(?:[-a-zA-Z\\d@:%._\\+.~#?&\\/=]*)$")) {
+            throw new InvalidURLException("Invalid link supplied. Please try again!!.");
+        }
+
+
+    }
+
 }
